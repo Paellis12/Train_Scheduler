f6d50ccffa55ffa0ee12a3ae03e52f6e5213cdda
Paellis12 <patrick.ellis84@yahoo.com>
2019-02-02 12:53:46 -0500

    changes

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function) {
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBKt0jTfVUeVTR_VqCU0MifrYq1TZsJxRg",
+    authDomain: "train-scheduler-50cbd.firebaseapp.com",
+    databaseURL: "https://train-scheduler-50cbd.firebaseio.com",
+    projectId: "train-scheduler-50cbd",
+    storageBucket: "",
+    messagingSenderId: "555787795230"
+  };
+  firebase.initializeApp(config);
+
+  
+
+
+}
